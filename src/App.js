e5550fa10e151ce062cf3704d6a66063530b8106
nareshgali <nareshgali1555@gmail.com>
2023-07-14 09:56:40 +0530

    Version 0.1 Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useAuth0 } from "@auth0/auth0-react";
+import Login from "./components/Login";
+import Logout from "./components/Logout";
+import Profile from "./components/Profile";
 
 function App() {
+  const { isLoading, error } = useAuth0();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {error && <p>Authentication Error</p>}
+      {!error && isLoading && <p>Loading...</p>}
+      {!error && !isLoading && (
+        <>
+          <Login />
+          <Logout />
+          <Profile />
+        </>
+      )}
     </div>
   );
 }
---
+++ src/components/Profile.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+
+const Profile = () => {
+  const { user, isAuthenticated } = useAuth0();
+  return (
+    isAuthenticated && (
+      <>
+        <h1>Profile</h1>
+        <article>
+          {user?.picture && <img src={user.picture} />}
+          <h2>{user?.name}</h2>
+          <ul>
+            {Object.keys(user).map((objKey, i) => (
+              <li key={i}>
+                {objKey} : {user[objKey]}
+              </li>
+            ))}
+          </ul>
+        </article>
+      </>
+    )
+  );
+};
+
+export default Profile;
